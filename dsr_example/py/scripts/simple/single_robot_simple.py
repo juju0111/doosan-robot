421ab592272b703d6ada0f457b28ef69fa772857
juju0111 <p3549823@gmail.com>
2022-08-07 22:25:34 +0900

    communicate with pytampgit status

--- dsr_example/py/scripts/simple/single_robot_simple.py
+++ dsr_example/py/scripts/simple/single_robot_simple.py
@@ -13,12 +13,12 @@ sys.path.append( os.path.abspath(os.path.join(os.path.dirname(__file__),"../../.
 
 # for single robot 
 ROBOT_ID     = "dsr01"
-ROBOT_MODEL  = "m1013"
+ROBOT_MODEL  = "a0912"
 import DR_init
 DR_init.__dsr__id = ROBOT_ID
 DR_init.__dsr__model = ROBOT_MODEL
 from DSR_ROBOT import *
-
+import numpy as np
 def shutdown():
     print("shutdown time!")
     print("shutdown time!")
@@ -119,10 +119,14 @@ if __name__ == "__main__":
 
     velx=[50, 50]
     accx=[100, 100]
-
+	
     p1= posj(0,0,0,0,0,0)                    #joint
     p2= posj(0.0, 0.0, 90.0, 0.0, 90.0, 0.0) #joint
-
+    p_ = np.array([0.0, 0.0, 45.0, 0.0, 50.0, 0.0])
+	
+    p3 = p_.tolist()
+	
+	
     x1= posx(400, 500, 800.0, 0.0, 180.0, 0.0) #task
     x2= posx(400, 500, 500.0, 0.0, 180.0, 0.0) #task
 
@@ -166,10 +170,11 @@ if __name__ == "__main__":
     b_list1 = [seg11, seg12, seg14, seg15, seg16] 
 
     while not rospy.is_shutdown():
-        movej(p2, vel=100, acc=100)
+        movej(p2, vel=100, acc=250)
        # movejx(x1, vel=30, acc=60, sol=0)
         #movel(x2, velx, accx)
-        movej(p1, vel=100, acc=100)
+        movej(p1, vel=150, acc=250)
+        movej(p3, vel=150, acc=250)	
        # movec(c1, c2, velx, accx)
        # movesj(qlist, vel=100, acc=100)
        # movesx(xlist, vel=100, acc=100)
